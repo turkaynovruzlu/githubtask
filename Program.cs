170416d24e5a5c344d459e1bb87efe3d3d92f254
turkaynovruzlu <tnovruzlu7682@gmail.com>
2024-05-03 11:57:56 +0400

    thirdcommit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //git init
+            //git status
+            //git add .
+            //git status
+            //git config user.name "turkaynovruzlu"
+            //git config user.email "tnovruzlu7682@gmail.com"
+            //git commit -m 'firstcommit'
+            //git branch
+            //git remote add origin https://github.com/turkaynovruzlu/githubtask.git
+            //git push -u origin master
         }
     }
 }
